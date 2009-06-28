638e0bcf2a42dd89d392078e2ebfa0c182c8a7da
Tymon Tobolski <i@teamon.eu>
2009-06-28 12:36:48 +0200

    Clean it up!

--- auth.rb
+++ auth.rb
@@ -1,22 +1,23 @@
-  def ensure_authenticated_as(klazz)
-    raise Unauthenticated unless session.user.is_a?(klazz)
-  end
+def ensure_authenticated_as(klazz)
+  raise Unauthenticated unless session.user.is_a?(klazz)
+end
 
-    def number_with_delimiter(number, delimiter = ".")
-      number.to_s.reverse.scan(/.{1,3}/).join(delimiter).reverse
-    end
-    
-    def truncate(text, max_length)
-      return text[0, max_length] + "..." if text.length > max_length
-      text
-    end
-    def colorize(var, format = nil)
-      color = if var.is_a?(Numeric)
-        var >= 0 ? :green : :red
-      else
-        var ? :green : :red
-      end
-      
-      var = format % var if format
-      '<span style="color: %s">%s</span>' % [color, var]
-    end
+def number_with_delimiter(number, delimiter = ".")
+  number.to_s.reverse.scan(/.{1,3}/).join(delimiter).reverse
+end
+
+def truncate(text, max_length)
+  return text[0, max_length] + "..." if text.length > max_length
+  text
+end
+
+def colorize(var, format = nil)
+  color = if var.is_a?(Numeric)
+    var >= 0 ? :green : :red
+  else
+    var ? :green : :red
+  end
+  
+  var = format % var if format
+  '<span style="color: %s">%s</span>' % [color, var]
+end
--- permalink.rb
+++
@@ -1,3 +0,0 @@
-def permalink(string)
-  Iconv.iconv('ascii//translit//IGNORE', 'utf-8', string).first.gsub("'", '').gsub(/[^a-zA-Z0-9-]+/, '-').gsub(/^-/, '').gsub(/-$/, '')
-end
